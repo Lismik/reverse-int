fb27d97bf966909c6ba5388120d969d11e4e50b6
Lismik <86889648+Lismik@users.noreply.github.com>
2022-01-17 21:47:20 +0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
-}
+    n = n + "";
+    if (n < 0) {
+        let y = n.split("").reverse().join("");
+        return +y.slice(0, -1);
+    } else {
+        return n.split("").reverse().join("");
+    }
+};
